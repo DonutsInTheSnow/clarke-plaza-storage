5c81f3cdca90fb9bbae70b0020f99783c99a0df9
Rich <rhudson4966@gmail.com>
2024-12-31 13:04:51 -0500

    update vite.config.js

--- client-vite/vite.config.js
+++ client-vite/vite.config.js
@@ -6,7 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:3001', 
+      // '/api': 'http://localhost:3001', 
+      '/api': 'https://clarke-plaza-storage-backend.vercel.app', 
     },
   },
 })
